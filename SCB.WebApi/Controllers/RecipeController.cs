44f986c67ea5785d2920ae57a8bd604a2fd05fb4
SaabW <woutersaab@gmail.com>
2025-04-24 23:23:47 +0200

    Finished recipe controller and service for all basic needs.

--- SCB.WebApi/Controllers/RecipeController.cs
+++ SCB.WebApi/Controllers/RecipeController.cs
@@ -1,3 +1,4 @@
+using RecipeServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SCB.WebApi.Controllers;
@@ -10,14 +11,111 @@ public class RecipeController : ControllerBase
     RecipeDataService recipeDataService = new();
 
     [HttpGet(Name = "Get all recipes")]
-    public List<Recipe> GetRecipes()
+    public ActionResult<List<Recipe>> GetRecipes()
     {
-        return recipeDataService.GetAllRecipes();
+        var recipes = recipeDataService.GetAllRecipes();
+
+        if(recipes is null || !recipes.Any())
+            return NotFound("There are no recipes present.");
+        else
+            return Ok(recipes);
+    }
+
+    [HttpGet("name/{name}", Name = "Get recipe by name")]
+    public ActionResult<Recipe?> GetRecipeByName(string name)
+    {
+        var recipes = recipeDataService.GetAllRecipes();
+        var recipe = recipeDataService.FindRecipeByName(name);
+
+        if (recipes == null || !recipes.Any())
+            return NotFound("There are no recipes");
+        
+        else if (recipe == null || !recipe.Any())
+            return NotFound($"No recipes found with name \"{name}\".");
+        
+        else
+            return Ok(recipe);
+    }
+
+    [HttpGet("name/{name}/version/{version}", Name = "Get recipe by name and version")]
+    public ActionResult<Recipe?> GetRecipeByNameAndVersion(string name, string version)
+    {
+        var recipes = recipeDataService.GetAllRecipes();
+        var recipe = recipeDataService.FindRecipeByNameAndVersion(name, version);
+
+        if (recipes == null || !recipes.Any())
+            return NotFound("There are no recipes");
+        else if (recipe == null)
+            return NotFound($"No recipes found with name \"{name}\" and version \"{version}\".");
+        else
+            return Ok(recipe);
     }
 
     [HttpPost(Name = "Add new recipe")]
-    public void AddNewRecipe(Recipe newRecipe)
+    public ActionResult AddNewRecipe(Recipe newRecipe)
     {
-        recipeDataService.AddNewRecipe(newRecipe);
+        //TODO: Add checks like: Same name and version already exist? Everything filled in? Invalid fraction (/0)? etc.
+        if(recipeDataService.FindRecipeByNameAndVersion(newRecipe.Name, newRecipe.Version) is not null)
+            return BadRequest($"A recipe with name \"{newRecipe.Name}\" and version \"{newRecipe.Version}\" already exists.");
+        else
+        {
+            recipeDataService.AddNewRecipe(newRecipe);
+            return Ok(newRecipe);
+        }
+    }
+
+    [HttpDelete(Name = "Delete all recipes")]
+    public ActionResult DeleteAllRecipes()
+    {
+        var recipes = recipeDataService.GetAllRecipes();
+
+        if (recipes == null || !recipes.Any())
+            return BadRequest("There are no recipes to be deleted.");
+        else
+        {
+            int recipesRemoved = recipes.Count();
+            recipeDataService.DeleteAllRecipes();
+            return Ok($"Removed all {recipesRemoved} recipes successfully.");
+        }
+    }
+
+    [HttpDelete("name/{name}", Name = "Delete recipes by name")]
+    public ActionResult DeleteRecipesByName(string name)
+    {
+        var recipes = recipeDataService.GetAllRecipes();
+        var recipe = recipeDataService.FindRecipeByName(name);
+
+        if (recipes == null || !recipes.Any())
+            return BadRequest("There are no recipes to be deleted.");
+        else if (recipe == null || !recipe.Any())
+            return NotFound($"No recipes found with name \"{name}\".");
+        else
+        {
+            int deletedRecipes = recipeDataService.DeleteRecipeByName(name);
+            if(deletedRecipes == 0)
+                return BadRequest("Recipe deletion went wrong...");
+            else
+                return Ok($"{deletedRecipes} recipes deleted.");
+        }       
+    }
+
+    [HttpDelete("name/{name}/version/{version}", Name = "Delete recipes by name and version")]
+    public ActionResult DeleteRecipeByNameAndVersion(string name, string version)
+    {
+        var recipes = recipeDataService.GetAllRecipes();
+        var recipe = recipeDataService.FindRecipeByNameAndVersion(name, version);
+
+        if (recipes == null || !recipes.Any())
+            return NotFound("There are no recipes");
+        else if (recipe == null)
+            return NotFound($"No recipes found with name \"{name}\" and version \"{version}\".");
+        else
+        {
+            int deletedRecipe = recipeDataService.DeleteRecipeByNameAndVersion(name, version);
+            if (deletedRecipe == 0)
+                return BadRequest("Recipe deletion went wrong...");
+            else
+                return Ok($"{deletedRecipe} recipes deleted.");
+        }
     }
 }
--- SCB.WebApi/Service/RecipeDataService.cs
+++ SCB.WebApi/Service/RecipeDataService.cs
@@ -1,6 +1,8 @@
 using System.IO;
 using System.Text.Json;
 
+namespace RecipeServices;
+
 public class RecipeDataService{
     private static readonly string jsonPath = Path.Combine("Data", "Recipes.json");
 
@@ -18,7 +20,7 @@ public class RecipeDataService{
         if(recipes is not null)
             return recipes;
         else
-            return [];
+            return new List<Recipe>();
     }
 
     public void AddNewRecipe(Recipe newRecipe)
@@ -33,5 +35,74 @@ public class RecipeDataService{
         string updatedJson = JsonSerializer.Serialize(recipes, jsonOptions);
 
         File.WriteAllText(jsonPath, updatedJson);
+    }
+
+    public List<Recipe>? FindRecipeByName(string nameRaw)
+    {
+        List<Recipe> recipes = GetAllRecipes();
+        List<Recipe> foundRecipes = new();
+        string name = nameRaw.ToLower();
+
+        foreach(var recipe in recipes)
+        {
+            if(recipe.Name.ToLower() == name)
+                foundRecipes.Add(recipe);
+        }
+        
+        if(foundRecipes.Count == 0)
+            return null;
+        else
+            return foundRecipes;
+    }
+
+    public Recipe? FindRecipeByNameAndVersion(string nameRaw, string versionRaw)
+    {
+        List<Recipe> recipes = GetAllRecipes();
+        string name = nameRaw.ToLower();
+        string version = versionRaw.ToLower();
+
+        foreach(var recipe in recipes)
+        {
+            if(recipe.Name.ToLower() == name && recipe.Version.ToLower() == version)
+                return recipe;
+        }
+        return null;
+    }
+    public void DeleteAllRecipes()
+    {
+        List<Recipe> emptyRecipes = new();
+
+        var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+        string updatedJson = JsonSerializer.Serialize(emptyRecipes, jsonOptions);
+        File.WriteAllText(jsonPath, updatedJson);
+    }
+
+    public int DeleteRecipeByName(string nameRaw)
+    {
+        List<Recipe> recipes = GetAllRecipes();
+        string name = nameRaw.ToLower();
+
+        int removed = recipes.RemoveAll(recipe => recipe.Name.ToLower() == name);
+
+        var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+        string updatedJson = JsonSerializer.Serialize(recipes, jsonOptions);
+        File.WriteAllText(jsonPath, updatedJson);
+
+        return removed; 
+    }
+
+    public int DeleteRecipeByNameAndVersion(string nameRaw, string versionRaw)
+    {
+        List<Recipe> recipes = GetAllRecipes();
+        string name = nameRaw.ToLower();
+        string version = versionRaw.ToLower();
+
+        int removed = recipes.RemoveAll(recipe => recipe.Name.ToLower() == name && recipe.Version.ToLower() == version);
+
+        var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+        string updatedJson = JsonSerializer.Serialize(recipes, jsonOptions);
+        File.WriteAllText(jsonPath, updatedJson);
+
+        return removed; 
     }
 }
