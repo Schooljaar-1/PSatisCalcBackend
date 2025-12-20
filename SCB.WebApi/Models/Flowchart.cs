3efff170e425c6c23191392902ee818d4c0ddc80
BaasW <wan.baas@student.avans.nl>
2025-12-20 21:34:34 +0100

    Created model of flowchart data.

---
+++ SCB.WebApi/Models/Flowchart.cs
@@ -0,0 +1,27 @@
+public class Node
+{
+    public required string Id {get; set;} //Unique indentifier of node
+    public required Position Position {get; set;} //Position of node inside of flowchart
+    public required NodeData NodeData {get; set;} //↓↓↓ See NodeData model class 
+}
+
+public class Edge
+{
+    public required string Id {get; set;} //Unique identifier of edge
+    public required string Source {get; set;} //Connection comes from which node?
+    public required string Target {get; set;} //Connection goes to which node?
+    public required string Label {get; set;} //The amount of units per minute that gets transferred between nodes
+}
+
+public class Position
+{
+    public float X {get; set;} //X-coordinate on flowchart
+    public float Y {get; set;} //Y-coordinate on flowchart
+}
+
+public class NodeData
+{
+    public required string Label {get; set;} //Machine type
+    public required string SubLabel {get; set;} //Amount of machines needed
+    public required string Image {get; set;} //Image of said machine
+}
