b88992820a29ccb07657b32fae3bc504abc6355e
BaasW <wan.baas@student.avans.nl>
2025-12-21 15:32:29 +0100

    Initial nodes and edges get made. Now to add the hardest logic...

--- SCB.WebApi/Service/FlowChartService.cs
+++ SCB.WebApi/Service/FlowChartService.cs
@@ -1,6 +1,7 @@
 namespace FlowchartServices;
 
 using System;
+using System.Reflection.Emit;
 
 public class FlowChartService{
     
@@ -13,22 +14,59 @@ public class FlowChartService{
     {
         List<Node> calculatedNodes = new();
         List<Edge> calculatedEdges = new();
+        var fractionService = new FractionService();
 
         int index = 0;
         foreach(var recipeAndAmount in recipesAndAmounts.Items)
         {
-            // Starting node (endproduct)
+            #region Starting nodes and edge
+            // Starting node 1 (endproduct)
+            var productSubLabel = recipeAndAmount.Amount.Fraction.Teller == 0
+                ? $"{recipeAndAmount.Amount.Integer}"
+                : $"{recipeAndAmount.Amount.Integer} {recipeAndAmount.Amount.Fraction.Teller}/{recipeAndAmount.Amount.Fraction.Noemer}";
             calculatedNodes.Add(new Node()
             {
-                Id=$"{recipeAndAmount.Recipe.Name}_{recipeAndAmount.Recipe.Version}",
+                Id=$"{recipeAndAmount.Recipe.Name}_{recipeAndAmount.Recipe.Version}_final_product{index}",
                 Position = new Position { X = 0f, Y = index * 100f },
                 NodeData = new NodeData
                 {
                     Label = recipeAndAmount.Recipe.Name,
-                    SubLabel = $"{recipeAndAmount.Amount.Integer + (double)recipeAndAmount.Amount.Fraction.Teller / recipeAndAmount.Amount.Fraction.Noemer:0.###}",
+                    SubLabel = productSubLabel,
                     Image = recipeAndAmount.Recipe.Image
                 }
             });
+
+            // Starting node 2 (machine to endproduct)
+            #region using fraction service
+            var recipeAmount = recipeAndAmount.Recipe.Amount;
+            var requestedFraction = fractionService.CompressFraction(recipeAndAmount.Amount);
+            var result = fractionService.Division(requestedFraction, recipeAmount);
+            var decompressed = fractionService.DecompressFraction(result);
+            var machineSubLabel = decompressed.Fraction.Teller == 0
+                ? $"{decompressed.Integer}"
+                : $"{decompressed.Integer} {decompressed.Fraction.Teller}/{decompressed.Fraction.Noemer}";
+            #endregion
+            calculatedNodes.Add(new Node()
+            {
+                Id=$"{recipeAndAmount.Recipe.Name}_{recipeAndAmount.Recipe.Version}_final_machine{index}",
+                Position = new Position { X = -300f, Y = index * 100f },
+                NodeData = new NodeData
+                {
+                    Label = recipeAndAmount.Recipe.Machine,
+                    SubLabel = machineSubLabel,
+                    Image = recipeAndAmount.Recipe.Machine
+                }
+            });
+            
+            // Starting edge
+            calculatedEdges.Add(new Edge(){
+                Id=$"{recipeAndAmount.Recipe.Name}_{recipeAndAmount.Recipe.Version}_machine_to_endproduct{index}",
+                Source=$"{recipeAndAmount.Recipe.Name}_{recipeAndAmount.Recipe.Version}_final_machine{index}",
+                Target=$"{recipeAndAmount.Recipe.Name}_{recipeAndAmount.Recipe.Version}_final_product{index}",
+                Label=$"{productSubLabel} {recipeAndAmount.Recipe.Name}s per minute"
+            });
+
+            #endregion
 
             // Filling a list of recipes referred as parts. 
 
